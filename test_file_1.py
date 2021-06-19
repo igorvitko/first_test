6d5b50a248c720e9a543a7c97393dd5e627648cc
Ihor <igor.vitko.74@gmail.com>
2021-06-19 21:47:56 +0300

    add super new line with def print

--- test_file_1.py
+++ test_file_1.py
@@ -5,6 +5,7 @@ b = m.pow(2, 16)
 print(a, b)
 print("first commit")
 c = "Hello world!"
+print(f"Your first programm - {c}")
 d = input("Введите свое имя: ")
 print('new line in editor')
 print(f"Hello, {d}!")
